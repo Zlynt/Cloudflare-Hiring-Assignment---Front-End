38313e7a76b7fb7e561cad1a8a27e85439d9d2d4
Zlynt <ivan.goncalo@gmail.com>
2021-11-03 23:49:55 +0000

    Hum
    So no Moment and no FontAwesome

--- src/Components/UserPost.js
+++ src/Components/UserPost.js
@@ -8,8 +8,8 @@ import {
     Card
 } from 'react-bootstrap';
 //import Moment from 'react-moment';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faClock, faHeart, faComment, faShare } from '@fortawesome/free-solid-svg-icons'
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+//import { faClock, faHeart, faComment, faShare } from '@fortawesome/free-solid-svg-icons'
 
 import YoutubeVideo, { YoutubeTools } from './YoutubeVideo';
 import SoundCloudAudio, { SoundCloudAudioTools } from './SoundCloudAudio';
@@ -53,8 +53,9 @@ const UserPost = ({ userName, userDescription, timestamp, title, description, ty
                     </Card.Header>
                     <Card.Body>
                         <div class="text-muted h7 mb-2">
-                            <FontAwesomeIcon icon={faClock} /> <spawn>{timestampToDate(timestamp)}</spawn>
-                            {/*<Moment fromNow unix>{timestamp / 1000}</Moment>*/}
+                            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="clock" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-clock fa-w-16"><path fill="currentColor" d="M256,8C119,8,8,119,8,256S119,504,256,504,504,393,504,256,393,8,256,8Zm92.49,313h0l-20,25a16,16,0,0,1-22.49,2.5h0l-67-49.72a40,40,0,0,1-15-31.23V112a16,16,0,0,1,16-16h32a16,16,0,0,1,16,16V256l58,42.5A16,16,0,0,1,348.49,321Z" class=""></path>
+                            </svg> <spawn>{timestampToDate(timestamp)}</spawn>
+                            {/*<FontAwesomeIcon icon={faClock} /><Moment fromNow unix>{timestamp / 1000}</Moment>*/}
                         </div>
                         <a class="card-link" href="#">
                             <h5 class="card-title">{title}</h5>
