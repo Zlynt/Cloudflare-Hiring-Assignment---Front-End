11bbd4c844f9cca023d309beb1c055f9394b9fe4
Zlynt <ivan.goncalo@gmail.com>
2021-11-03 22:05:22 +0000

    Here we go again
    Why Cloudflare? My project compiles in local machine.... In Cloudflares pages not... Why?

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React, { useState, useEffect } from 'react';
 
 import Container from 'react-bootstrap/Container';
-import Jumbotron from 'react-bootstrap/Jumbotron';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-import Navbar from './Components/Navbar';
-import UserPostSubmit from './Components/UserPostSubmit';
-import PostList from './Components/PostList';
 
 import './App.scss';
 
 function App() {
   return (
     <div>
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">Social Network</Navbar.Brand>
-      </Navbar>
       <Container className="p-3 gedf-wrapper">
         <ToastContainer />
-        {/*<UserPostSubmit /> */}
-        {/*<PostList />*/}
       </Container>
     </div>
   );
